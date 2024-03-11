c615935bcadd4e079793e2daf703fa00f5a6fb35
chxpz <rpcweb3@gmail.com>
2024-03-11 14:39:48 -0300

    chore: uncomment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,21 +2,21 @@ import { ethers } from "hardhat";
 
 async function main() {
   const owner = "0x51BfB0d94b98428e1bb87c4EDAFbD93366D2Ab71";
-  const wethAddress = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14";
+  const wethAddress = "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889";
 
   const UniswapMarket = await ethers.deployContract(
     "UniswapMarketUniversalRouter"
   );
   await UniswapMarket.waitForDeployment();
 
-  // const ERC20Swapper = await ethers.deployContract("ERC20Swapper", [
-  //   owner,
-  //   wethAddress,
-  //   UniswapMarket.target,
-  // ]);
-  // await ERC20Swapper.waitForDeployment();
+  const ERC20Swapper = await ethers.deployContract("ERC20Swapper", [
+    owner,
+    wethAddress,
+    UniswapMarket.target,
+  ]);
+  await ERC20Swapper.waitForDeployment();
 
-  // console.log(`ERC20Swapper deployed to ${ERC20Swapper.target}`);
+  console.log(`ERC20Swapper deployed to ${ERC20Swapper.target}`);
 
   console.log(`UniswapMarket deployed to ${UniswapMarket.target}`);
 }
